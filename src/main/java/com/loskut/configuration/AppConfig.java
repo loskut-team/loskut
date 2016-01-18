9db81d2980b614408f1eafa1474c6932f8f9db40
Rayant <reyantt@gmail.com>
2016-01-18 23:56:46 +0200

    resources

--- src/main/java/com/loskut/configuration/AppConfig.java
+++ src/main/java/com/loskut/configuration/AppConfig.java
@@ -6,22 +6,15 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.loskut")
-public class AppConfig extends WebMvcConfigurationSupport {
+public class AppConfig extends WebMvcConfigurerAdapter {
 
-	@Override
-	protected void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-		configurer.enable();
-	}
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
